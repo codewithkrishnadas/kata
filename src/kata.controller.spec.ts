475a6de8b459ea01c8d506341c4fbc9021077ee7
KD <krishna@composolution.com>
2023-05-31 22:45:51 +0530

    first commit

---
+++ src/kata.controller.spec.ts
@@ -0,0 +1,34 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { AppController } from './kata.controller';
+
+describe('AppController', () => {
+  let appController: AppController;
+
+  beforeEach(async () => {
+    const app: TestingModule = await Test.createTestingModule({
+      controllers: [AppController],
+      providers: [],
+    }).compile();
+
+    appController = app.get<AppController>(AppController);
+  });
+
+  describe('step -- 1 ', () => {
+    it('add "" should be 0', () => {
+      expect(appController.add()).toBe(0);
+    });
+
+    it('add "1" should be 1', () => {
+      expect(appController.add()).toBe(1);
+    });
+
+    it('add "1,2" should be 1', () => {
+      expect(appController.add()).toBe(3);
+    });
+
+    it('add "1, 2" should be 1', () => {
+      expect(appController.add()).toBe(3);
+    });
+
+  });
+});
---
+++ src/kata.controller.ts
@@ -0,0 +1,10 @@
+import { Controller, Get } from '@nestjs/common';
+
+@Controller()
+export class AppController {
+  constructor() {}
+
+  add(): number {
+    return 0;
+  }
+}
